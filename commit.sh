f237d5c2249406c3b352ea41a18bd0b16c509385
talesCPV <tales@flexibus.com.br>
2024-06-03 16:36:50 -0300

    by_script

--- commit.sh
+++ commit.sh
@@ -15,8 +15,8 @@ then
 
     git commit -m "by_script"
 
-    git branch -M main
-    git remote add origin git@github.com:talesCPV/lince.git
+#    git branch -M main
+#    git remote add origin git@github.com:talesCPV/lince.git
     git remote set-url origin git@github.com:talesCPV/lince.git
 
     git push -u -f origin main
